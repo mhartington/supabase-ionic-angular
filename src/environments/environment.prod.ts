7313ea12623594fc381545f871acf33eba8648e1
Mike Hartington <mikehartington@gmail.com>
2022-03-16 15:56:13 -0400

    cleanup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: "https://azjucypbdhyyoghsapbr.supabase.co",
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6anVjeXBiZGh5eW9naHNhcGJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDczNTU4MjYsImV4cCI6MTk2MjkzMTgyNn0.hRfUTY9BVXfLx6yUEYFZNXCNRLN9-UqG0ER6huQE8nU"
+  supabaseUrl: "",
+  supabaseKey: ""
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: "https://azjucypbdhyyoghsapbr.supabase.co",
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6anVjeXBiZGh5eW9naHNhcGJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDczNTU4MjYsImV4cCI6MTk2MjkzMTgyNn0.hRfUTY9BVXfLx6yUEYFZNXCNRLN9-UqG0ER6huQE8nU"
+  supabaseUrl: "",
+  supabaseKey: ""
 };
 
 /*
